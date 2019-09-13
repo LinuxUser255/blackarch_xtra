0cd7c02dcbfa6485d210326af033a60ffa31ffef
Chris <atnl@protonmail.com>
2019-09-13 00:38:27 -0400

    Add files via upload

---
+++ wallchanger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Wallpaper change in black arch.
+#Be sure that the file path leads
+#to the downloaded location of the image you want.
+#This has been tested in BlackArch using XTERM Terminal.
+#It's best to move or copy this to the root directory before use.
+
+
+feh --bg-scale /root/Downloads/wallpaper.png
+
+
